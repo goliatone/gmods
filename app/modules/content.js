b2512c9deb0509fa36ad402a79e868c522c69768
goliatone <burgosemi@gmail.com>
2013-11-17 18:08:50 -0500

    Content Module: Add rudimentary and crappy content manager, just for testing sake.

--- app/modules/content.js
+++ app/modules/content.js
@@ -1,12 +1,26 @@
 define('content',['jquery'], function($){
 
+	var articles = {
+		"home": "<p>This is article one, nothing more</p>",
+		"about": "<p>This is the content</p>",
+		"contact": "<p>This is the 3rd article, so... nothing should be shocking by now!</p>"
+	};
+
 	var Content = function(config){
 		this.init(config);
 	};
 	Content.id = 'content';
 	Content.prototype.init = function(config){
 		this.config = config;
+		$(window).on('navigate', (this.update).bind(this));
 		console.log(this.constructor.id, ' initialized ', config );
+	};
+
+	Content.prototype.update = function(e, data){
+		console.log('update ', data.url)
+		var content = articles[data.url];
+		var article = $('<article></article>').html(content).data('url', data.url);
+		console.log(this.config.el.append(article));
 	};
 
 	return {
