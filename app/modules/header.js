20337180667bfba4d9d6eb378779ff5ede992d60
goliatone <burgosemi@gmail.com>
2013-11-17 18:09:14 -0500

    Content Header: Add rudimentary and crappy content manager, just for testing sake.

--- app/modules/header.js
+++ app/modules/header.js
@@ -9,6 +9,12 @@ define('header',['jquery'], function($){
 		console.log(this.constructor.id, ' initialized ', config );
 		config.el.find('a').each(function(){
 			console.log(this.href)
+			$(this).on('click',function(e){
+				e.preventDefault();			
+				console.log('SOmething happende!', e, e.currentTarget.pathname);
+				$(window).trigger('navigate', [{url:e.currentTarget.pathname.replace('/','')}]);
+			});
+			
 		});
 	};
 	//This is the module's interface with the loader.
@@ -18,6 +24,10 @@ define('header',['jquery'], function($){
 			console.log(this.id);
 		},
 		factory:function(config){
+			//Here we can have logic, do we want to 
+			//have more than one instance, or only one
+			//per app? Instead of doing a singleton, 
+			//we manage this internally.
 			return new Header(config);
 		}
 	};
