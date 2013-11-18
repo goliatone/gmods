083ecdd39410005a90ff548c4fb67f615b5b2a56
goliatone <burgosemi@gmail.com>
2013-11-18 16:55:22 -0500

    Main: Updated requirejs, import bootstrap.

--- app/main.js
+++ app/main.js
@@ -2,12 +2,20 @@
 /* jshint strict: false */
 requirejs.config({
     paths: {
-        'jquery': '../jquery/jquery',
-        'gmods': '../gmods'
+        // 'jquery':['//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js', '../jquery/jquery'],
+        'jquery':'../jquery/jquery',
+        'gmods': '../gmods',
+        // 'bootstrap': ['//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.2/js/bootstrap.min.js', '../bootstrap/dist/js/bootstrap']
+        'bootstrap': '../bootstrap/dist/js/bootstrap'
+    },
+    shim:{
+    	'bootstrap':{
+    		deps:['jquery']
+    	}
     }
 });
 
-define(['gmods', 'jquery'], function (GMods, $) {
+define(['gmods', 'jquery', 'bootstrap'], function (GMods, $) {
     console.log('Loading');
 	var gmods = new GMods({container:'main', selector:'[data-module]'});
 	gmods.init();
